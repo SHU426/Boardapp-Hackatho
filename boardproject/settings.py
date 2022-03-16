3bb8b4e3e51568775187fd6280a46c83b7ef0cbf
SHU426 <56482033+SHU426@users.noreply.github.com>
2022-03-16 17:26:13 +0900

    delete setting

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,4 +131,3 @@ MEDIA_URL = 'media/'
 
 LOGIN_URL = 'login'
 
-django_heroku.setting(locals())
